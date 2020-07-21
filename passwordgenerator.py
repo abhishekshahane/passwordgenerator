b5b6cedaf4e4f7be458a5a6a6fe2d10970804f48
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-07-21 18:03:45 +0530

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,7 +1,8 @@
 import random
 getnum = int(input("Pick a number greater than 7:  "))
 if (getnum <= 7):
-    print("Run again.")
+    print("Error 205: Too many characters entered")
+    print("Run again using python passwordgenerator.py, or click the run button on your IDE.")
     exit()
 lista = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0','#', '@', '!', '%','^', '//', '\\']
 def main(lista, getnum):
